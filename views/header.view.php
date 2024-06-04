28a35e718607adf04e75c63d3cee5b57d6603449
noedel <noe.dwwm48@gmail.com>
2024-06-04 15:14:03 +0200

    header + titre
    background basique

--- views/header.view.php
+++ views/header.view.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
     <title>projetAward</title>
 </head>
-<body>
+<body class="body">
+<header class="header">
+    <h1 class="header__title">DW/WM Awards</h1>
+</header>
