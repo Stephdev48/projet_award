59a7d246caee1350b8e3de11600dd70f4b4923c4
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2024-06-14 15:34:54 +0200

    board admin

--- views/admin.view.php
+++ views/admin.view.php
@@ -1,18 +1,16 @@
-<div class="container resultats_apprenants">
+<div class="container section_admin">
     <div class="row">
         <div class="col-3">
 
         </div>
-        <div class="col-6">
-            <table>
-                <caption>
-                    Gestion des votants
-                </caption>
+        <div class="col-6 m-5">
+            <h2 class="text-center fs-2">Tableau de gestion des votants</h2>
+            <table class="">
                 <thead>
                     <tr>
-                        <th scope="col">Email</th>
-                        <th scope="col">Statut</th>
-                        <th scope="col">Modification du statut</th>
+                        <th scope="col" class="tableau">Email</th>
+                        <th scope="col" class="tableau">Statut</th>
+                        <th scope="col" class="tableau">Modification du statut</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -23,12 +21,14 @@
                         foreach ($votes as $key => $value){
                         ?>
                         <tr>
-                            <th scope="row"><?php echo $value['email']; ?></th>
-                            <th scope="row"><?php echo $value['statut']; ?></th>
-                            <td><?php 
+                            <th scope="row" class="tableau"><?php echo $value['email']; ?></th>
+                            <th scope="row" class="tableau"><?php echo $value['statut']; ?></th>
+                            <td class="tableau"><?php 
                                 if($value['statut'] == 1){
                                     echo "<a href='index.php?page=togglevote&email=".$value['email']."&statut=0'>Unvalidate</a>";
-                                }else{ echo "<a href='index.php?page=togglevote&email=".$value['email']."&statut=1'>Validate</a>" ;}?></td>
+                                }else{ 
+                                    echo "<a href='index.php?page=togglevote&email=".$value['email']."&statut=1'>Validate</a>" ;
+                                }?></td>
                         </tr>
                         <?php
                         }
